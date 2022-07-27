0bc183ed03d9f0d7df23cab3800b307d528a14d1
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-07-27 14:47:25 +0900

    NextJS blog with markdown posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import { getSortedPostsData } from "../lib/post";
 import homeStyles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
   return (
-    <div>
+    <div className={homeStyles.container}>
       <Head>
         <title>BokDol Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,10 +26,30 @@ const Home: NextPage = () => {
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
